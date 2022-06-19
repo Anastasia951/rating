a918f7e1a4cdd455e99ffa9e950a6b5dcc1acffa
Anastasia951 <n.ivanova504@gmail.com>
2022-06-20 01:09:27 +0300

    grid

--- layouts/Footer/Footer.tsx
+++ layouts/Footer/Footer.tsx
@@ -4,7 +4,19 @@ import styles from './Footer.module.scss'
 import cn from 'classnames'
 
 const Footer = ({ ...props }: IFooter): JSX.Element => {
-  return <div {...props}>Footer</div>
+  return (
+    <div {...props}>
+      <div className={styles.copy}>OwlTop © 2020 - 2021 Все права защищены</div>
+      <ul className={styles.docs}>
+        <li>
+          <a href=''>Пользовательское соглашение</a>
+        </li>
+        <li>
+          <a href=''>Политика конфиденциальности</a>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Footer
